4a89f130166c32c0fc18c3d12f6899e561c74470
Mykola Yaremchuk <mykola@idalko.com>
2017-07-11 00:42:52 +0300

    updated user_card creation sql query

--- src/main/resources/db_script/user_card_creation.sql
+++ src/main/resources/db_script/user_card_creation.sql
@@ -1,15 +1,20 @@
-CREATE TABLE user_card (
-id bigserial NOT NULL PRIMARY KEY,
-nick_name varchar(20) NOT NULL,
-first_name varchar(20) DEFAULT NULL,
-last_name varchar(20) DEFAULT NULL,
-phone varchar(20) DEFAULT NULL,
-email varchar(20) DEFAULT NULL,
-skype varchar(20) DEFAULT NULL,
-address_id varchar(20) DEFAULT NULL,
-user_password varchar(20) DEFAULT NULL,
-gender varchar(20) DEFAULT NULL,
-date_of_birth varchar(20) DEFAULT NULL,
-avatarUrl varchar(20) DEFAULT NULL,
-creation_date timestamp DEFAULT NULL
-);
+create table user_card
+(
+  id serial not null
+    constraint user_card_pkey
+    primary key,
+  first_name varchar(50) default 'DefaultFirstName'::character varying not null,
+  last_name varchar(80) default 'DefaultLastName'::character varying not null,
+  password varchar(80) not null,
+  email varchar(100) not null
+    constraint uk_9vo0r76325y30g44ol4s0t6sq
+    unique,
+  date_of_birth timestamp default now(),
+  address_id varchar(30),
+  avatar_url varchar(100) default 'http://example-avatar-url.elect.com'::character varying,
+  gender varchar(10) default 'Male'::character varying,
+  phone varchar(20) default '+77-777-7777-777'::character varying,
+  registered_on timestamp default now(),
+  skype varchar(30) default 'skype-example'::character varying
+)
+;
