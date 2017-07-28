6cd6f23f948812848e8c3233509b96c726c09e9f
Mykola Yaremchuk <mykola@idalko.com>
2017-07-29 00:11:11 +0300

    used lombock @Data annotation for all entities + modified README file

--- src/main/java/com/elect/model/Address.java
+++ src/main/java/com/elect/model/Address.java
@@ -1,13 +1,15 @@
 package com.elect.model;
 
 import org.hibernate.annotations.ColumnDefault;
-
+import lombok.Data;
 import javax.persistence.*;
+
 
 /**
  * Created by Mykola Yaremchuk on 7/11/17.
  */
 @Entity
+@Data
 @Table( name = "address" )
 public class Address {
 
@@ -30,80 +32,4 @@ public class Address {
     @Column(name = "country", nullable = false, length = 50)
     @ColumnDefault("'DefaultCountry'")
     private String country;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
-
-    public String getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(String streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Address address = (Address) o;
-
-        if (id != address.id) return false;
-        if (streetName != null ? !streetName.equals(address.streetName) : address.streetName != null) return false;
-        if (streetNumber != null ? !streetNumber.equals(address.streetNumber) : address.streetNumber != null)
-            return false;
-        if (city != null ? !city.equals(address.city) : address.city != null) return false;
-        return country != null ? country.equals(address.country) : address.country == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (streetName != null ? streetName.hashCode() : 0);
-        result = 31 * result + (streetNumber != null ? streetNumber.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "id=" + id +
-                ", streetName='" + streetName + '\'' +
-                ", streetNumber='" + streetNumber + '\'' +
-                ", city='" + city + '\'' +
-                ", country='" + country + '\'' +
-                '}';
-    }
 }
